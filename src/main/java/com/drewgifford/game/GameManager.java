7f0eb8a672a3845cafd2f347f3cb857461e1ee41
kai lo <kailo@Mini-Mac.local>
2018-10-06 10:22:33 +0300

    added go message at countdown end

--- src/main/java/com/drewgifford/game/GameManager.java
+++ src/main/java/com/drewgifford/game/GameManager.java
@@ -143,6 +143,8 @@ public class GameManager {
 					for(Player p : Bukkit.getOnlinePlayers()){
 						setIngame(p, true);
 						p.setLevel(0);
+						p.playSound(p.getLocation(), Sound.CLICK, 1f, 2f);
+						p.sendMessage(ChatColor.YELLOW + "GO!");
 					}
 					startGame(pl.registeredGame);
 					this.cancel();
