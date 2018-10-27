0bd66b0d3683ded8cba5e854273c2158614ceb7d
ExcelCyphernia <kai@enoshlo.com>
2018-10-27 08:31:28 +0300

    fixed minor issues with throwing items

--- src/main/java/com/drewgifford/event/EventSelectKit.java
+++ src/main/java/com/drewgifford/event/EventSelectKit.java
@@ -12,41 +12,32 @@ import com.drewgifford.CypherniaMinigames;
 import com.drewgifford.game.Kit;
 
 public class EventSelectKit implements Listener {
-	
+
 	private CypherniaMinigames cm;
 	public EventSelectKit(CypherniaMinigames cm) {
 		this.cm = cm;
 	}
-	
+
 	@EventHandler
-    public void clickItem(InventoryClickEvent e){
-        Inventory i = e.getClickedInventory();
+	public void clickItem(InventoryClickEvent e){
+		try {
+			Inventory i = e.getClickedInventory();
+			if(!i.getName().equalsIgnoreCase(cm.color("&8&l&nKit Selector"))) return;
+			ItemStack item = e.getCurrentItem();
+			String name = item.getItemMeta().getDisplayName();
+			Player p = (Player) e.getWhoClicked();
+			for(Kit k : cm.getKits()){
+				ItemStack x = k.getBuiltIcon();
 
-        if(!i.getName().equalsIgnoreCase(cm.color("&8&l&nKit Selector"))) return;
+				if(name.equalsIgnoreCase(x.getItemMeta().getDisplayName())){
 
-        ItemStack item = e.getCurrentItem();
-        String name = item.getItemMeta().getDisplayName();
-        Player p = (Player) e.getWhoClicked();
+					Kit.setSelectedKit(p, k);
 
-        for(Kit k : cm.getKits()){
-            ItemStack x = k.getBuiltIcon();
-
-            if(name.equalsIgnoreCase(x.getItemMeta().getDisplayName())){
-
-                Kit.setSelectedKit(p, k);
-
-                p.sendMessage(cm.color("&6Selected kit &e"+ ChatColor.stripColor(name)));
-                p.closeInventory();
-
-
-
-
-
-            }
-            e.setCancelled(true);
-
-        }
-
-
-    }
+					p.sendMessage(cm.color("&6Selected kit &e"+ ChatColor.stripColor(name)));
+					p.closeInventory();
+				}
+				e.setCancelled(true);
+			}
+		} catch (Exception ex) {}
+	}
 }
