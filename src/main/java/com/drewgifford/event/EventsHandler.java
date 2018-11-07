6f9615b99deae58ce75f861f1a01ee69b03f26a4
ExcelCyphernia <kai@enoshlo.com>
2018-11-07 16:00:36 +0200

    made event listeners public

--- src/main/java/com/drewgifford/event/EventsHandler.java
+++ src/main/java/com/drewgifford/event/EventsHandler.java
@@ -6,17 +6,35 @@ import com.drewgifford.CypherniaMinigames;
 
 public class EventsHandler {
 
-    public EventsHandler(CypherniaMinigames plugin){
+	public static EventJoin joinEvent;
+	public static EventLeave leaveEvent;
+	public static EventDamage damageEvent;
+	public static EventRightClick rightClickEvent;
+	public static EventHunger hungerEvent;
+	public static EventSelectKit selectKitEvent;
+	public static EventPickupItem pickupItemEvent;
+	public static EventBlockBreak blockBreakEvent;
 
-        PluginManager pm = plugin.getServer().getPluginManager();
+	public EventsHandler(CypherniaMinigames plugin){
 
-        pm.registerEvents(new EventJoin(plugin), plugin);
-        pm.registerEvents(new EventLeave(plugin), plugin);
-        pm.registerEvents(new EventDamage(plugin), plugin);
-        pm.registerEvents(new EventRightClick(plugin), plugin);
-        pm.registerEvents(new EventHunger(plugin), plugin);
-        pm.registerEvents(new EventSelectKit(plugin), plugin);
-        pm.registerEvents(new EventPickupItem(plugin), plugin);
-        pm.registerEvents(new EventBlockBreak(plugin), plugin);
-    }
+		joinEvent = new EventJoin(plugin);
+		leaveEvent = new EventLeave(plugin);
+		damageEvent = new EventDamage(plugin);
+		rightClickEvent = new EventRightClick(plugin);
+		hungerEvent = new EventHunger(plugin);
+		selectKitEvent = new EventSelectKit(plugin);
+		pickupItemEvent = new EventPickupItem(plugin);
+		blockBreakEvent = new EventBlockBreak(plugin);
+
+		PluginManager pm = plugin.getServer().getPluginManager();
+
+		pm.registerEvents(joinEvent, plugin);
+		pm.registerEvents(leaveEvent, plugin);
+		pm.registerEvents(damageEvent, plugin);
+		pm.registerEvents(rightClickEvent, plugin);
+		pm.registerEvents(hungerEvent, plugin);
+		pm.registerEvents(selectKitEvent, plugin);
+		pm.registerEvents(pickupItemEvent, plugin);
+		pm.registerEvents(blockBreakEvent, plugin);
+	}
 }
